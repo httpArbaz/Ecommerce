a6d1064dc0b9b1d8eb1deaba28072d65aedfd487
Suleman Ahmed <48311202+Suleman3015@users.noreply.github.com>
2022-12-17 22:21:42 +0500

    pushes

--- src/components/reusableThings/storeButton/storeButton.js
+++ src/components/reusableThings/storeButton/storeButton.js
@@ -1,27 +1,24 @@
 import React, { useState } from "react";
-import Popover from "@mui/material/Popover";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import "./index.css";
 
 function StoreButton() {
-  const [anchorEl, setAnchorEl] = useState(null);
+  const [active, setActive] = useState(false);
 
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+
+    setActive(true);
   };
 
   const handleClose = () => {
-    setAnchorEl(null);
- 
+
+    setActive(false);
   };
 
-  console.log(anchorEl);
-
-  const open = Boolean(anchorEl);
-  const id = open ? "simple-popover" : undefined;
+ 
   return (
-    <>
+    <div >
       <a
         style={{
           display: "flex",
@@ -29,54 +26,38 @@ function StoreButton() {
         }}
         href="#Categories"
         onMouseEnter={handleClick}
+        onClick={() => setActive(!active)} 
         className="anchor"
       >
-        Store {anchorEl == null ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />}
+        Store {active === true ? <ArrowDropDownIcon   /> : <ArrowDropUpIcon  />}
       </a>
-  
-      <Popover
-       onClick={handleClose}
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "left",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            padding: "10px",
-            justifyContent: "space-evenly",
-          }}
-           onMouseLeave={handleClose}
-        >
-          <a href="/" onClick={handleClose} className="anchorIn">
+
+      {active === true ? (
+        <div onMouseLeave={handleClose} className="menuHover">
+          <a href="/" onClick={handleClose} className="anchorIn ">
             {" "}
             Home and Kitchen
           </a>
-          <a href="/" className="anchorIn">
+          <a href="/" className="anchorIn ">
             Sports
           </a>
-          <a href="/" className="anchorIn">
+          <a href="/" className="anchorIn ">
             {" "}
             Out Door
           </a>
-          <a href="/" className="anchorIn">
+          <a href="/" className="anchorIn ">
             Pet Supplies
           </a>
-          <a href="/" className="anchorIn">
+          <a href="/" className="anchorIn ">
             {" "}
             Arts and Crafts
           </a>
-          <a href="/" className="anchorIn">
+          <a href="/" className="anchorIn ">
             Garden and Outdoor
           </a>
         </div>
-      </Popover>
-    </>
+      ) : null}
+    </div>
   );
 }
 
