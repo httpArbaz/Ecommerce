a3ee275d368a4508ccf8c66317c06ae989600694
Suleman Ahmed <48311202+Suleman3015@users.noreply.github.com>
2022-12-24 17:33:41 +0500

    pushes

--- src/Pages/item.js
+++ src/Pages/item.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import ItemDetail from '../components/item/itemDetail'
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Feature from '../components/feautureProduct/feature';
+import { useLocation } from "react-router-dom";
+
 
 function Item() {
   const matchesPro = useMediaQuery('(max-width:500px)');
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div style={{ backgroundColor:"#f5f5f5"}}>
     <div style={{width:matchesPro === true ? "80%" : "70%",margin:"auto",paddingTop : matchesPro === true ? "150px"  :"250px"}}>
